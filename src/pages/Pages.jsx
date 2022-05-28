b5784c7235c99a455a1a0179661900ab7b9e52f5
farhan-sharif <farhansharif1410@gmail.com>
2022-05-28 21:40:17 +0600

    first

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../component/mainpage/Home'
+
+const Pages = () => {
+  return (
+    <>
+        <h1>home</h1>
+        <Home/>
+    </>
+  )
+}
+
+export default Pages
