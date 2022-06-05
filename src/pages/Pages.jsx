bc31ba3b6d59fdc4b2513f9da1c202059d7d98c4
farhan-sharif <farhansharif1410@gmail.com>
2022-06-05 23:11:31 +0600

    TopCart

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import FlashDeals from '../component/flashDeals/FlashDeals'
 import Home from '../component/mainpage/Home'
+import TopCate from '../component/Top/TopCate'
+
 
 const Pages = ({productItems, CartItem, addToCart}) => {
   return (
     <>
         <Home CartItem={CartItem} />
         <FlashDeals productItems={productItems}  addToCart={addToCart}/>
+        <TopCate/>
     </>
   )
 }
