de891855bf424c58ab78e7a89adce94278ff7ca2
farhan-sharif <farhansharif1410@gmail.com>
2022-06-06 22:36:35 +0600

    Discount

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Discount from '../component/discount/Discount'
 import FlashDeals from '../component/flashDeals/FlashDeals'
 import Home from '../component/mainpage/Home'
+import NewArrivals from '../component/newarrivals/NewArrivals'
 import TopCate from '../component/Top/TopCate'
 
 
@@ -10,6 +12,8 @@ const Pages = ({productItems, CartItem, addToCart}) => {
         <Home CartItem={CartItem} />
         <FlashDeals productItems={productItems}  addToCart={addToCart}/>
         <TopCate/>
+        <NewArrivals/>
+        <Discount/>
     </>
   )
 }
