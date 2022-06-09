7473b0b64e3ec502c302157e3b28c7b2079485d6
farhan-sharif <farhansharif1410@gmail.com>
2022-06-09 22:26:18 +0600

    shopCart

--- src/component/shop/ShopCart.jsx
+++ src/component/shop/ShopCart.jsx
@@ -1,30 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 
-const ShopCart = ({shopItems, addToCart}) => {
-  const [count, setCount] =useState(0)
+const ShopCart = ({ shopItems, addToCart }) => {
+  const [count, setCount] = useState(0)
   const increment = () => {
-    setCount(count + 1 )
+    setCount(count + 1)
   }
-
 
   return (
     <>
-      {shopItems.map((shopItems)=> {
-        return(
-          
-          <div className="box">
-            <div className="product mtop">
-              <div className="img">
-                <span className='discount'>{shopItems.discount}% off</span>
-                <img src={shopItems.cover} alt="" />
-                <div className="product-like">
-                  <label>0</label>
+      {shopItems.map((shopItems, index) => {
+        return (
+          <div className='box'>
+            <div className='product mtop'>
+              <div className='img'>
+                <span className='discount'>{shopItems.discount}% Off</span>
+                <img src={shopItems.cover} alt='' />
+                <div className='product-like'>
+                  <label>{count}</label> <br />
                   <i className='fa-regular fa-heart' onClick={increment}></i>
+                </div>
+              </div>
+              <div className='product-details'>
+                <h3>{shopItems.name}</h3>
+                <div className='rate'>
+                  <i className='fa fa-star'></i>
+                  <i className='fa fa-star'></i>
+                  <i className='fa fa-star'></i>
+                  <i className='fa fa-star'></i>
+                  <i className='fa fa-star'></i>
+                </div>
+                <div className='price'>
+                  <h4>${shopItems.price}.00 </h4>
+                  {/* step : 3  
+                     if hami le button ma click garryo bahne 
+                    */}
+                  <button onClick={() => addToCart(shopItems)}>
+                    <i className='fa fa-plus'></i>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          
         )
       })}
     </>
